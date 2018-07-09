29e9391974a450800c8c258745e58ec323c8bac2
James Boyer <aesthetics1@gmail.com>
2018-07-09 14:24:27 -0700

    Added complexity to Q01

--- CTCI.Questions/Chapter01/Question01.cs
+++ CTCI.Questions/Chapter01/Question01.cs
@@ -6,6 +6,8 @@ namespace CTCI.Questions.Chapter01
 	{
 		// Implement an algorithm to determine if a string has all unique characters. What
 		// if you cannot use additional data structures?
+
+		// Time complexity is O(n), space complexity is O(n)
 		public static bool HasUniqueCharacters(string s)
 		{
 			HashSet<char> characterSet = new HashSet<char>();
