5a6a3dac111370de7bd595d80a7b5151d507dad2
James Boyer <aesthetics1@gmail.com>
2018-07-11 16:32:20 -0700

    Added second c01/q01 method and explanations

--- CTCI.Questions/Chapter01/Question01.cs
+++ CTCI.Questions/Chapter01/Question01.cs
@@ -4,8 +4,15 @@ namespace CTCI.Questions.Chapter01
 {
 	public class Question01
 	{
-		// Implement an algorithm to determine if a string has all unique characters. What
-		// if you cannot use additional data structures?
+		// Implement an algorithm to determine if a string has all unique characters.
+
+		// I decided first to use a hash set, because a hashset does not allow duplicate
+		// values. The hash function is used to be able to find a character in the string
+		// without iterating through a list or other similar data structure. I then add
+		// each character in the string to the hash set and monitor the count of to see if
+		// it has increased. If it has, this means we have added a unique character, and
+		// should continue checking the string. If the count does not increase, we have
+		// found a duplicate and should return false.
 
 		// Time complexity is O(n), space complexity is O(n)
 		public static bool HasUniqueCharacters(string s)
@@ -21,6 +28,40 @@ namespace CTCI.Questions.Chapter01
 				if (countBefore == characterSet.Count)
 				{
 					return false;
+				}
+			}
+
+			return true;
+		}
+
+		// ...What if you cannot use additional data structures?
+
+		// If you cannot use additional data structures, using an array of true/false 
+		// values can work. If you know the number of characters in your character set
+		// (0-255 for ascii for example) you can use a fixed size array. Check each char
+		// and flip from false --> true when a character is found. If a value is already
+		// true when it is visited, you have found a duplicate value.
+		// This method turns out to be much faster than the first method. It is easy to 
+		// see why. We are not running a hashing algorithm against every value. The fixed
+		// size array will also never have to change in size. Most data structures double
+		// in size once the maximum capacity has been reached. We will not have this 
+		// overhead with an array.
+		// Note: This is the solution in the book, implemented in C# instead of Java.
+
+		// Time complexity is O(n), space complexity is O(n)
+		public static bool HasUniqueCharacters2(string s)
+		{
+			bool[] characterSet = new bool[256];
+
+			foreach (char c in s)
+			{
+				if (characterSet[(int)c])
+				{
+					return false;
+				}
+				else
+				{
+					characterSet[(int)c] = true;
 				}
 			}
 
--- CTCI.UnitTests/Chapter01Tests.cs
+++ CTCI.UnitTests/Chapter01Tests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using CTCI.Questions.Chapter01;
 using Xunit;
 
@@ -10,20 +11,38 @@ namespace CTCI.UnitTests
 		[Fact]
 		public void Question01Should()
 		{
-			// a, a
 			Assert.False(Question01.HasUniqueCharacters("aa"));
+			Assert.False(Question01.HasUniqueCharacters2("aa"));
 
-			// Several letters are the same
-			Assert.False(Question01.HasUniqueCharacters("This is just a word string"));
+			Assert.False(Question01.HasUniqueCharacters("This is just a string"));
+			Assert.False(Question01.HasUniqueCharacters2("This is just a string"));
 
-			// a and A are different characters
 			Assert.True(Question01.HasUniqueCharacters("aA"));
+			Assert.True(Question01.HasUniqueCharacters2("aA"));
 
-			// all unique
 			Assert.True(Question01.HasUniqueCharacters("abcdefg"));
+			Assert.True(Question01.HasUniqueCharacters2("abcdefg"));
 
-			// spaces are not distinct
 			Assert.False(Question01.HasUniqueCharacters("  "));
+			Assert.False(Question01.HasUniqueCharacters2("  "));
+
+			// Add all ascii characters to a string to check
+			string allAsciiCharacters = "";
+
+			for (int i = 0; i <= 255; i++)
+			{
+				char c = (char)i;
+				allAsciiCharacters += c;
+			}
+
+			Assert.True(Question01.HasUniqueCharacters(allAsciiCharacters));
+			Assert.True(Question01.HasUniqueCharacters2(allAsciiCharacters));
+
+			// Add a duplicate character to check again
+			allAsciiCharacters += (char)0;
+
+			Assert.False(Question01.HasUniqueCharacters(allAsciiCharacters));
+			Assert.False(Question01.HasUniqueCharacters2(allAsciiCharacters));
 		}
 
 		[Fact]
