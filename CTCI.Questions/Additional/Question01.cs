76b40da8d5b50ae7d7b3c07df16bd0eece7c0564
James Boyer <aesthetics1@gmail.com>
2020-02-08 22:30:55 -0800

    Added editorconfig file, added comments to question

--- CTCI.Questions/Additional/Question01.cs
+++ CTCI.Questions/Additional/Question01.cs
@@ -1,9 +1,16 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace CTCI.Questions.Additional
 {
 	// Given two sorted arrays, merge them together in a sorted manner.
+
+	// The idea here was to essentially merge the two arrays together by comparing one item 
+	// from each array at a time in order to provide O(n) time complexity. We compare one
+	// item from each array and add the lower item to the new array. If one array runs out
+	// of items first, we can assume that the rest of the items left in the other array are
+	// all higher so we can fill the remaining slots with it.
+
 	public class Question01
 	{
 		public static int[] MergeAndSort(int[] first, int[] second)
