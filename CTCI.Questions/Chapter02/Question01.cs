82855cf3b1df2ea666a7ecbc85fce2ca1f619224
James Boyer <aesthetics1@gmail.com>
2020-02-09 18:48:34 -0800

    Added follow up to question 1 without using any additional buffer

--- CTCI.Questions/Chapter02/Question01.cs
+++ CTCI.Questions/Chapter02/Question01.cs
@@ -4,11 +4,28 @@ using CTCI.DataStructures;
 namespace CTCI.Questions.Chapter02
 {
 	// Write code to remove duplicates from an unsorted linked list.
+
+	// A naive approach to this problem might be to iterate over the list once while keeping
+	// track of all of the duplicates and then iterating over it a second time to clean them
+	// all up. A better solution might be to keep track of which elements we have seen
+	// before while iterating over the list and then removing any duplicates as we see them.
+	// We can use a dictionary to quickly check whether or not we have seen an element
+	// before.
+
+	// A follow-up to this question asks: How would you solve this problem if a temporary
+	// buffer is not allowed?
+
+	// Removing the temporary buffer makes the question a little trickier. If the idea is to
+	// favor space-efficiency over time-efficiency, we could use two iterators. The first
+	// would traverse the list one-by-one, giving a pointer to the current element we're
+	// checking. The second would traverse the remaining elements of the list, looking for
+	// dupes matching the first iterator and removing them along the way.
+
 	public class Question01<T>
 	{
 		public static void RemoveDuplicates(DataStructures.LinkedList<T> linkedList)
 		{
-			if(linkedList.Head == null)
+			if (linkedList.Head == null)
 			{
 				return;
 			}
@@ -24,7 +41,7 @@ namespace CTCI.Questions.Chapter02
 				Node<T> previous = iterator;
 				iterator = iterator.Next;
 
-				if(seen.ContainsKey(iterator.Value))
+				if (seen.ContainsKey(iterator.Value))
 				{
 					previous.Next = iterator.Next;
 				}
@@ -35,5 +52,34 @@ namespace CTCI.Questions.Chapter02
 			}
 		}
 
+		public static void RemoveDuplicatesNoBuffer(DataStructures.LinkedList<T> linkedList)
+		{
+			if (linkedList.Head == null)
+			{
+				return;
+			}
+
+			Node<T> backIterator = linkedList.Head;
+
+			while (backIterator != null)
+			{
+				Node<T> forwardIterator = backIterator.Next;
+				Node<T> previousNode = backIterator;
+
+				while (forwardIterator != null)
+				{
+					// If we have found a dupe, remove it from the list.
+					if (forwardIterator.Value.Equals(backIterator.Value))
+					{
+						previousNode.Next = forwardIterator.Next;
+					}
+
+					previousNode = previousNode.Next;
+					forwardIterator = forwardIterator.Next;
+				}
+
+				backIterator = backIterator.Next;
+			}
+		}
 	}
 }
--- CTCI.UnitTests/Chapter02Tests.cs
+++ CTCI.UnitTests/Chapter02Tests.cs
@@ -40,6 +40,38 @@ namespace CTCI.UnitTests
 		}
 
 		[Fact]
+		public void Question01NoBufferShould()
+		{
+			var linkedList = new LinkedList<int>();
+
+			linkedList.AppendToTail(1);
+			linkedList.AppendToTail(2);
+			linkedList.AppendToTail(2);
+			linkedList.AppendToTail(3);
+			linkedList.AppendToTail(3);
+
+			Question01<int>.RemoveDuplicatesNoBuffer(linkedList);
+
+			var expected = new LinkedList<int>();
+
+			expected.AppendToTail(1);
+			expected.AppendToTail(2);
+			expected.AppendToTail(3);
+
+			int expectedFirst = expected.Head.Value;
+			int expectedSecond = expected.Head.Next.Value;
+			int expectedThird = expected.Head.Next.Next.Value;
+
+			int actualFirst = linkedList.Head.Value;
+			int actualSecond = linkedList.Head.Next.Value;
+			int actualThird = linkedList.Head.Next.Next.Value;
+
+			Assert.Equal(expectedFirst, actualFirst);
+			Assert.Equal(expectedSecond, actualSecond);
+			Assert.Equal(expectedThird, actualThird);
+		}
+
+		[Fact]
 		public void Question02Should()
 		{
 			var linkedList = new LinkedList<int>();
