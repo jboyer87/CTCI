507af8b577087240f63c53d674f16a5690cb7a4f
James J. Boyer <james.boyer87@gmail.com>
2020-02-12 16:59:50 -0800

    Answered question 5

--- CTCI.Questions/Chapter02/Question05.cs
+++ CTCI.Questions/Chapter02/Question05.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 namespace CTCI.Questions.Chapter02
 {
 	public class Question05
@@ -6,7 +7,35 @@ namespace CTCI.Questions.Chapter02
 		public static DataStructures.Node<T> GetNodeAtBeginningOfLoop<T>(
 			DataStructures.LinkedList<T> linkedList)
 		{
+			if(linkedList.Head == null)
+			{
+				throw new ArgumentException("Cannot evaluate an empty list.");
+			}
 
+			DataStructures.Node<T> slowRunner = linkedList.Head;
+			DataStructures.Node<T> fastRunner = linkedList.Head;
+
+
+			while(true)
+			{
+				// Jump ahead one
+				slowRunner = slowRunner.Next;
+
+				if(fastRunner.Next != null)
+				{
+					// Jump ahead two
+					fastRunner = fastRunner.Next.Next;
+				}
+				else
+				{
+					throw new ArgumentException("List does not have a loop.");
+				}
+
+				if(slowRunner == fastRunner)
+				{
+					return slowRunner;
+				}
+			}
 		}
 	}
 }
